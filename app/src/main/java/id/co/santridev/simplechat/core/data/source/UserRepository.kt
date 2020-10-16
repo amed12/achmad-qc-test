9e5558fd30330d3c368839e9b60c466c4226f800
amed12 <amedixro@gmail.com>
2020-10-16 23:46:09 +0700

    feat : describe function login in repository

--- app/src/main/java/id/co/santridev/simplechat/core/data/source/UserRepository.kt
+++ app/src/main/java/id/co/santridev/simplechat/core/data/source/UserRepository.kt
@@ -1,18 +1,23 @@
 /*
  * *
- *  * Created by Achmad Fathullah on 10/16/20 11:05 PM
+ *  * Created by Achmad Fathullah on 10/16/20 11:46 PM
  *  * Copyright (c) 2020 . All rights reserved.
- *  * Last modified 10/16/20 11:05 PM
+ *  * Last modified 10/16/20 11:45 PM
  *
  */
 
 package id.co.santridev.simplechat.core.data.source
 
 import android.content.Context
+import com.qiscus.sdk.chat.core.QiscusCore
+import com.qiscus.sdk.chat.core.data.model.QiscusAccount
 import id.co.santridev.simplechat.core.data.source.pref.PreferenceHelper
 import id.co.santridev.simplechat.core.domain.model.User
 import id.co.santridev.simplechat.core.domain.repository.IUserRepository
 import id.co.santridev.simplechat.core.utils.Action
+import id.co.santridev.simplechat.core.utils.AvatarUtil
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class UserRepository private constructor(private val context: Context) : IUserRepository {
     private val preferenceHelpers by lazy {
@@ -31,6 +36,20 @@ class UserRepository private constructor(private val context: Context) : IUserRe
         onSuccess: Action<User>,
         onError: Action<Throwable>
     ) {
-        TODO("Not yet implemented")
+        QiscusCore.setUser(email, password)
+            .withUsername(name)
+            .withAvatarUrl(AvatarUtil.generateAvatar(name))
+            .save()
+            .map { this.mapFromQiscusAccount(it) }
+            .doOnNext { this.setCurrentUser(it) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ onSuccess.call(it) }) { onError.call(it) }
     }
+
+    private fun mapFromQiscusAccount(qiscusAccount: QiscusAccount): User = User(
+        id = qiscusAccount.email,
+        name = qiscusAccount.username,
+        avatarUrl = qiscusAccount.avatar
+    )
 }
