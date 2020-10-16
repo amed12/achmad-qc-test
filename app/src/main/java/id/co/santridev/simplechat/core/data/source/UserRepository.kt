1f85b8b6a556edeac427d3aaf4033d7a087f6e3b
amed12 <amedixro@gmail.com>
2020-10-16 23:05:35 +0700

    feat : create interface function login to repository

--- app/src/main/java/id/co/santridev/simplechat/core/data/source/UserRepository.kt
+++ app/src/main/java/id/co/santridev/simplechat/core/data/source/UserRepository.kt
@@ -1,8 +1,8 @@
 /*
  * *
- *  * Created by Achmad Fathullah on 10/16/20 10:57 PM
+ *  * Created by Achmad Fathullah on 10/16/20 11:05 PM
  *  * Copyright (c) 2020 . All rights reserved.
- *  * Last modified 10/16/20 10:57 PM
+ *  * Last modified 10/16/20 11:05 PM
  *
  */
 
@@ -12,6 +12,7 @@ import android.content.Context
 import id.co.santridev.simplechat.core.data.source.pref.PreferenceHelper
 import id.co.santridev.simplechat.core.domain.model.User
 import id.co.santridev.simplechat.core.domain.repository.IUserRepository
+import id.co.santridev.simplechat.core.utils.Action
 
 class UserRepository private constructor(private val context: Context) : IUserRepository {
     private val preferenceHelpers by lazy {
@@ -23,4 +24,13 @@ class UserRepository private constructor(private val context: Context) : IUserRe
     }
 
     override fun getCurrentUser(): User = preferenceHelpers.getCurrentUser()
+    override fun login(
+        email: String,
+        password: String,
+        name: String,
+        onSuccess: Action<User>,
+        onError: Action<Throwable>
+    ) {
+        TODO("Not yet implemented")
+    }
 }
