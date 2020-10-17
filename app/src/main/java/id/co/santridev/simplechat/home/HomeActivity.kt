7c1e9d3804597155dbe73bd61dd2220f558a3956
amed12 <amedixro@gmail.com>
2020-10-17 17:59:42 +0700

    feat : create ui home

---
+++ app/src/main/java/id/co/santridev/simplechat/home/HomeActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * *
+ *  * Created by Achmad Fathullah on 10/17/20 5:59 PM
+ *  * Copyright (c) 2020 . All rights reserved.
+ *  * Last modified 10/17/20 5:56 PM
+ *
+ */
+
+package id.co.santridev.simplechat.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.co.santridev.simplechat.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
