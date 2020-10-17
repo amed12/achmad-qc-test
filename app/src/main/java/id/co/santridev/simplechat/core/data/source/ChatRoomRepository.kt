4645487fa36d98f7b96be06586a349cb5aa7bf7d
amed12 <amedixro@gmail.com>
2020-10-17 23:38:40 +0700

    feat : describe getChatRoom on ChatRoomRepository.kt

--- app/src/main/java/id/co/santridev/simplechat/core/data/source/ChatRoomRepository.kt
+++ app/src/main/java/id/co/santridev/simplechat/core/data/source/ChatRoomRepository.kt
@@ -1,21 +1,41 @@
 /*
  * *
- *  * Created by Achmad Fathullah on 10/17/20 11:22 PM
+ *  * Created by Achmad Fathullah on 10/17/20 11:38 PM
  *  * Copyright (c) 2020 . All rights reserved.
- *  * Last modified 10/17/20 11:22 PM
+ *  * Last modified 10/17/20 11:38 PM
  *
  */
 
 package id.co.santridev.simplechat.core.data.source
 
+import com.qiscus.sdk.chat.core.QiscusCore
 import com.qiscus.sdk.chat.core.data.model.QiscusChatRoom
+import com.qiscus.sdk.chat.core.data.remote.QiscusApi
 import id.co.santridev.simplechat.core.domain.model.User
 import id.co.santridev.simplechat.core.domain.repository.IChatRoomRepository
 import id.co.santridev.simplechat.core.utils.Action
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class ChatRoomRepository : IChatRoomRepository {
     override fun getChatRoom(onSuccess: Action<List<QiscusChatRoom>>, onError: Action<Throwable>) {
-        TODO("Not yet implemented")
+        Observable.from(QiscusCore.getDataStore().getChatRooms(100))
+            .filter { chatRoom -> chatRoom.lastComment != null }
+            .toList()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ onSuccess.call(it) }) { onError.call(it) }
+
+        QiscusApi.getInstance()
+            .getAllChatRooms(true, false, true, 1, 100)
+            .flatMap { Observable.from(it) }
+            .doOnNext { qiscusChatRoom -> QiscusCore.getDataStore().addOrUpdate(qiscusChatRoom) }
+            .filter { chatRoom -> chatRoom.lastComment.id.toInt() != 0 }
+            .toList()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ onSuccess.call(it) }) { onError.call(it) }
     }
 
     override fun createChatRoom(
