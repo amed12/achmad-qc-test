6b77380e358d66876114e56d56af6bb692fee304
amed12 <amedixro@gmail.com>
2020-10-17 12:28:34 +0700

    refactor : change login presenter to login viewmodel

--- app/src/main/java/id/co/santridev/simplechat/login/LoginViewModel.kt
+++ app/src/main/java/id/co/santridev/simplechat/login/LoginViewModel.kt
@@ -1,13 +1,16 @@
 /*
  * *
- *  * Created by Achmad Fathullah on 10/17/20 1:30 AM
+ *  * Created by Achmad Fathullah on 10/17/20 12:28 PM
  *  * Copyright (c) 2020 . All rights reserved.
- *  * Last modified 10/17/20 1:30 AM
+ *  * Last modified 10/17/20 12:28 PM
  *
  */
 
 package id.co.santridev.simplechat.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import id.co.santridev.simplechat.core.domain.model.User
 import id.co.santridev.simplechat.core.domain.usecase.IUserUseCase
 import id.co.santridev.simplechat.core.utils.Action
@@ -19,35 +22,43 @@ interface ILoginPresenter {
     fun showErrorMessage(errorMessage: String)
 }
 
-class LoginPresenter(
-    private val presenterListener: ILoginPresenter,
+class LoginViewModel(
     private val userUseCase: IUserUseCase
-) {
+) : ViewModel() {
+    private var _stateHomeLiveData = MutableLiveData<Boolean>()
+    private var _errorStringLiveData = MutableLiveData<String>()
+    private var _stateLoadingLiveData = MutableLiveData<Boolean>()
+    var stateHomeData: LiveData<Boolean> = _stateHomeLiveData
+    var stateLoading: LiveData<Boolean> = _stateLoadingLiveData
+    var errorMessage: LiveData<String> = _errorStringLiveData
+
     fun start() {
         userUseCase.getCurrentUser(onSuccess = object : Action<User> {
             override fun call(t: User) {
                 if (t.id.isNotEmpty())
-                    presenterListener.showHomePage()
+                    _stateHomeLiveData.postValue(true)
+                else
+                    _stateHomeLiveData.postValue(false)
             }
         }, onError = object : Action<Throwable> {
             override fun call(t: Throwable) {
-                presenterListener.showErrorMessage(t.message.toString())
+                _errorStringLiveData.postValue(t.message.toString())
             }
         })
     }
 
     fun login(email: String, password: String, name: String) {
-        presenterListener.showLoading()
+        _stateLoadingLiveData.postValue(true)
         userUseCase.login(email, password, name, onSuccess = object : Action<User> {
             override fun call(t: User) {
-                presenterListener.dismissLoading()
-                presenterListener.showHomePage()
+                _stateLoadingLiveData.postValue(false)
+                _stateHomeLiveData.postValue(true)
             }
 
         }, onError = object : Action<Throwable> {
             override fun call(t: Throwable) {
-                presenterListener.dismissLoading()
-                presenterListener.showErrorMessage(t.message.toString())
+                _stateLoadingLiveData.postValue(false)
+                _errorStringLiveData.postValue(t.message.toString())
             }
 
         })
